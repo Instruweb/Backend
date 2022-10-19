bc4c49c120ff0a877baa21dec191b87d46ee1da8
Nick Welles <nickwelles@live.nl>
2022-10-19 15:10:24 +0200

    added maincategory entity

---
+++ instruweb-backend/src/main/java/com/instruweb/domain/MainCategory.java
@@ -0,0 +1,23 @@
+package com.instruweb.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "main_category")
+public class MainCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public MainCategory() {}
+
+    public MainCategory(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {return id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+}
